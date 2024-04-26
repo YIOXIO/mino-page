6c519e0bd6136412e003be81b9a5fd283f52dd34
unknown <C:\The Bat!>
2024-04-26 16:37:02 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -44,7 +44,6 @@ const swiper = new Swiper('.sample-slider', {
   })
 
 
-
   var speakers = new Swiper('.speakers__slider', {
     grabCursor:true,    
     loop: true,
@@ -89,6 +88,11 @@ const swiper = new Swiper('.sample-slider', {
 });
 
 
+
+
+
+
+// Якорные ссылки
 document.querySelectorAll('a.header__nav-link_anchor').forEach(link => {
   link.addEventListener('click', function(e) {
       e.preventDefault();
@@ -123,7 +127,9 @@ document.querySelectorAll('a.header__nav-link_anchor').forEach(link => {
       }
   });
 });
+// 
 
+// JS для бургера
 document.querySelector('.burger-menu').addEventListener('click', function () {
   document.querySelector('.side-navbar').classList.toggle('side-navbar-open');
 });
@@ -133,166 +139,76 @@ const burgerMenu = document.querySelector('.burger-menu');
 burgerMenu.addEventListener('click', function() {
   this.classList.toggle('active');
 });
+// __________
 
 
 
-
-//JS для модального окна
-// const popup = document.querySelector('.popup');
-// const buttonClosePopup = document.querySelector('.popup__close');
-
-// function openPopup(popupElement) {
-//     popupElement.classList.add('popup_is_active');
-//     popupElement.addEventListener('click', handleOverlayClosePopup);
-//     document.addEventListener('keydown', handleEscClosePopup);
-// }
-
-// function closePopup(popupElement) {
-//     popupElement.classList.remove('popup_is_active');
-//     popupElement.removeEventListener('click', handleOverlayClosePopup);
-//     document.removeEventListener('keydown', handleEscClosePopup);
-// }
-
-// function handleOverlayClosePopup(evt) {
-//     if (evt.target === evt.currentTarget) {
-//         closePopup(evt.currentTarget);
-//     }
-// }
-
-// function handleEscClosePopup(evt) {
-//     if (evt.key === 'Escape') {
-//         closePopup(document.querySelector('.popup_is_active'));
-//     }
-// }
-
-// if(buttonClosePopup){
-//   buttonClosePopup.addEventListener('click', () => closePopup(popup));
-// }
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   let currentStep = 1;
-//   const stepForms = document.querySelectorAll('#stepForm .step');
-
-//   function hideStep(step) {
-//       stepForms[step - 1].style.display = 'none';
-//   }
-
-//   function showStep(step) {
-//       stepForms[step - 1].style.display = 'block';
-//   }
-
-//   function switchToStep(step) {
-//       hideStep(currentStep);
-//       currentStep = step;
-//       showStep(currentStep);
-//   }
-
-//   document.getElementById('stepForm').addEventListener('click', function(event) {
-//       const element = event.target;
-//       if (element.nodeName === 'BUTTON') {
-//           if (element.dataset.next) {
-//               switchToStep(Number(element.dataset.next));
-//           } else if (element.dataset.prev) {
-//               switchToStep(Number(element.dataset.prev));
-//           }
-//       }
-//   });
-
-//   document.addEventListener('wpcf7mailsent', function(event) {
-
-//       switchToStep(2); 
-//   }, false);
-// });
-
-// document.addEventListener('wpcf7mailfailed', function(event) {
-//   var errorPopup = document.querySelector('.popup__error');
-//   openPopup(errorPopup);
-// }, false);
-
-
-
-
-// // Начало кода для табов
-document.addEventListener('DOMContentLoaded', function() {
-  var tabs = document.querySelectorAll('.tab-header-item');
-  var contents = document.querySelectorAll('.tab-content-item');
-
-  tabs.forEach(function(tab) {
-    tab.addEventListener('click', function() {
-      tabs.forEach(function(t) {
-        t.classList.remove('active');
-      });
-      contents.forEach(function(content) {
-        content.classList.remove('active');
-      });
-      this.classList.add('active');
-      var content = document.querySelector('.tab-content-item[data-content="'+this.getAttribute('data-tab')+'"]');
-      content.classList.add('active');
-    });
-  });
-});
- //Конец кода для табов 
-
- function handleFormNavigation() {
-  document.querySelectorAll('.tab-content-item').forEach(function(tabContent) {
-    let currentStep = 1;
-    const stepForms = tabContent.querySelectorAll('.step');
-
-    function hideStep(step) {
-        stepForms[step - 1].style.display = 'none';
-    }
-
-    function showStep(step) {
-        stepForms[step - 1].style.display = 'block';
-    }
-
-    function switchToStep(step) {
-        hideStep(currentStep);
-        currentStep = step;
-        showStep(currentStep);
-    }
-
-    tabContent.querySelector('.step').addEventListener('click', function(event) {
-        const element = event.target;
-        if (element.nodeName === 'BUTTON') {
-            if (element.dataset.next) {
-                switchToStep(Number(element.dataset.next));
-            } else if (element.dataset.prev) {
-                switchToStep(Number(element.dataset.prev));
-            }
-        }
-    });
-
-    document.addEventListener('wpcf7mailsent', function(event) {
-        switchToStep(2); 
-        var tabs = document.querySelectorAll('.tab-header-item');
-        tabs.forEach(function(tab) {
-            tab.style.display = 'none';
-        });
-    }, false);
-  });
-}
-
-function handleTabNavigation() {
-  var tabs = document.querySelectorAll('.tab-header-item');
-  var contents = document.querySelectorAll('.tab-content-item');
-  tabs.forEach(function(tab) {
-      tab.addEventListener('click', function() {
-          tabs.forEach(function(t) {
-              t.classList.remove('active');
-          });
-          contents.forEach(function(content) {
-              content.classList.remove('active');
-          });
-          this.classList.add('active');
-          var content = document.querySelector('.tab-content-item[data-content="'+this.getAttribute('data-tab')+'"]');
-          content.classList.add('active');
-      });
-  });
-}
 
 document.addEventListener('DOMContentLoaded', function() {
-  handleFormNavigation();
-  handleTabNavigation();
+  var radioButtons = document.querySelectorAll('input[name="radio-708"]');
+  var hiddenInput = document.querySelector('input[name="hidden-link"]');
+
+  if (radioButtons && hiddenInput) {
+      radioButtons.forEach(function(radio) {
+          radio.addEventListener('change', function() {
+              var value = this.value;
+              switch (value) {
+                  case 'Ректор':
+                      hiddenInput.value = 'https://xn----8sbokcxee2ae3a.xn--p1ai/%d0%be%d0%bf%d1%80%d0%be%d1%81%d0%bd%d1%8b%d0%b8-%d0%bb%d0%b8%d1%81%d1%82/%d1%80%d0%b5%d0%ba%d1%82%d0%be%d1%80/';
+                      break;
+                  case 'Cтратегическое развитие*':
+                      hiddenInput.value = 'https://xn----8sbokcxee2ae3a.xn--p1ai/%d0%be%d0%bf%d1%80%d0%be%d1%81%d0%bd%d1%8b%d0%b8-%d0%bb%d0%b8%d1%81%d1%82/%d1%81%d1%82%d1%80%d0%b0%d1%82/';
+                      break;
+                  case 'Финансово-экономическая деятельность*':
+                      hiddenInput.value = 'https://xn----8sbokcxee2ae3a.xn--p1ai/%d0%be%d0%bf%d1%80%d0%be%d1%81%d0%bd%d1%8b%d0%b8-%d0%bb%d0%b8%d1%81%d1%82/%d1%84%d0%b8%d0%bd/';
+                      break;
+                  default:
+                      hiddenInput.value = '';
+                      break;
+              }
+          });
+      });
+  }
 });
+
+
+const popup = document.querySelector('.popup');
+const buttonClosePopup = document.querySelector('.popup__close');
+
+function openPopup(popupElement) {
+    popupElement.classList.add('popup_is_active');
+    popupElement.addEventListener('click', handleOverlayClosePopup);
+    document.addEventListener('keydown', handleEscClosePopup);
+}
+
+function closePopup(popupElement) {
+    popupElement.classList.remove('popup_is_active');
+    popupElement.removeEventListener('click', handleOverlayClosePopup);
+    document.removeEventListener('keydown', handleEscClosePopup);
+}
+
+function handleOverlayClosePopup(evt) {
+    if (evt.target === evt.currentTarget) {
+        closePopup(evt.currentTarget);
+    }
+}
+
+function handleEscClosePopup(evt) {
+    if (evt.key === 'Escape') {
+        closePopup(document.querySelector('.popup_is_active'));
+    }
+}
+
+if(buttonClosePopup){
+  buttonClosePopup.addEventListener('click', () => closePopup(popup));
+}
+
+document.addEventListener('wpcf7mailsent', function(event) {
+  var successPopup = document.querySelector('.popup__success');
+  openPopup(successPopup);
+}, false);
+
+document.addEventListener('wpcf7mailfailed', function(event) {
+  var errorPopup = document.querySelector('.popup__error');
+  openPopup(errorPopup);
+}, false);
